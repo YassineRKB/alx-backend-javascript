24523f167d268fa758e93ea63a5da51c4bafcd55
YassineRKB <yassine@arcraven.com>
2024-01-04 00:55:06 +0100

    task 11

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -5,7 +5,9 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
