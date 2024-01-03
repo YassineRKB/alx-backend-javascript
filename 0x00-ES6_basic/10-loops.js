417c9efd9c3d9153ec310d4c1420149c6390d948
YassineRKB <yassine@arcraven.com>
2024-01-04 00:50:15 +0100

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const ChuckNorrisArray = [];
+  for (const chuck of array) {
+    ChuckNorrisArray.push(appendString + chuck);
+  }
+
+  return ChuckNorrisArray;
+}
