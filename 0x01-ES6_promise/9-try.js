14f5b695d3345e3a3ca58eb813c6a466abd6f910
YassineRKB <yassine@arcraven.com>
2024-01-04 02:21:00 +0100

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
