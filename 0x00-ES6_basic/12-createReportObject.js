b3bf533a9075bd50a9096da4a863cb42340189ab
YassineRKB <yassine@arcraven.com>
2024-01-04 01:14:25 +0100

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments() { return Object.keys(employeesList).length; },
+  };
+}
