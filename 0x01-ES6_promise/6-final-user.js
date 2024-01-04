86f4b9bb426ece9897a2b96ec3e3f9d680fe0705
YassineRKB <yassine@arcraven.com>
2024-01-04 02:13:26 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((results) => results.map(
+      ({ status, value, reason }) => ({
+        status,
+        value: status === 'fulfilled' ? value : reason.toString(),
+      }),
     ));
 }
