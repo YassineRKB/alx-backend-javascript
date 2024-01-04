6763a51650a1727b3f7cc6cafd45b1296949b2ee
YassineRKB <yassine@arcraven.com>
2024-01-04 01:47:40 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  try {
-    const [photo, user] = Promise.all([uploadPhoto(), createUser()]);
-    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  } catch (error) {
-    console.error('Signup system offline');
-  }
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    }).catch(() => (console.log('Signup system offline')));
 }
