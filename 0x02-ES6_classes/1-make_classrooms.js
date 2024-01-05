7597abaa602ab96f5791c2a3a091666abe147cd7
YassineRKB <yassine@arcraven.com>
2024-01-05 02:10:21 +0100

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const valarr = [19, 20, 34];
+  return valarr.map((value) => new ClassRoom(value));
+}
