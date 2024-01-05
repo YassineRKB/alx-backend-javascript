10681c053c357f77e1c479f170d1bae663e3b572
YassineRKB <yassine@arcraven.com>
2024-01-05 02:21:34 +0100

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+export default class Currency {
+  /* eslint-disable no-underscore-dangle */
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() { return this._code; }
+
+  get name() { return this._name; }
+
+  set code(code) { this._code = code; }
+
+  set name(name) { this._name = name; }
+
+  displayFullCurrency() { return `${this.name} (${this.code})`; }
+}
