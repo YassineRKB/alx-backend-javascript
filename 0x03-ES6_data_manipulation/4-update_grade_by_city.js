3fc1a40291aa686b12aa925b4d49d5ac64bdc4d4
YassineRKB <yassine@arcraven.com>
2024-01-10 02:57:54 +0100

    4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+export default function updateStudentGradeByCity(studentsList, targetLocation, updatedGrades) {
+  return studentsList
+    .filter(({ location }) => location === targetLocation)
+    .map((student) => {
+      const matchingGrades = updatedGrades.find(({ studentId }) => studentId === student.id);
+      const { grade = 'N/A' } = matchingGrades || {};
+      return { ...student, grade };
+    });
+}
