36f9d185c9202352fa3eb5c8ca351d39d45a23d5
YassineRKB <yassine@arcraven.com>
2024-01-10 03:13:55 +0100

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,8 +3,10 @@ export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || !startString) {
     return '';
   }
+
   const filteredValues = Array.from(set)
     .filter(value => value && value.startsWith(startString))
     .map(value => value.slice(startString.length));
+
   return filteredValues.join('-');
 }
