5c3ce4cfba1be35d0c8499c2f1d7988bb29a58e6
YassineRKB <yassine@arcraven.com>
2024-01-10 03:25:58 +0100

    11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+
+  if (count >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+}
