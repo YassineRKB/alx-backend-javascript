28ad917567171ccb3a20fe15995fb49eadf2fa9d
YassineRKB <yassine@arcraven.com>
2024-01-10 03:02:28 +0100

    5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+export default function createInt8TypedArray(len, pos, value) {
+  if (pos >= len) {
+    throw new Error('Position outside range');
+  }
+
+  const nArray = new Int8Array(len).fill(0);
+  nArray[pos] = value;
+  return new DataView(nArray.buffer);
+}
