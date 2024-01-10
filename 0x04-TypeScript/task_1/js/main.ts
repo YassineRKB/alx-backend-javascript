4b75bc11ecf794861c17661f80ff219556aee791
YassineRKB <yassine@arcraven.com>
2024-01-11 00:48:10 +0100

    ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,19 @@ export interface Teacher {
   [index:string]: any,
 }
 
+export interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
