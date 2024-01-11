8f4b2765435ed07695afea0cb70e168680d2c2b7
YassineRKB <yassine@arcraven.com>
2024-01-11 01:19:00 +0100

    task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,3 +42,11 @@ export const createEmployee = (salary: number | string): Director | Teacher => {
   if (typeof salary === 'number' && salary < 500) return new Teacher();
   return new Director();
 };
+// task 6: fix ca error
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) return employee.workDirectorTasks();
+  return employee.workTeacherTasks();
+}
