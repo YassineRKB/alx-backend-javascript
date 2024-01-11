dbebbaab0d195626ffa4aaab0350fd88b9f27c22
YassineRKB <yassine@arcraven.com>
2024-01-11 01:03:15 +0100

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,25 @@ export interface printTeacherFunction {
 export function printTeacher(firstName: string, lastName: string) {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentC {
+  firstName: string,
+  lastName: string,
+  workOnHomework(): string,
+  displayName(): string,  
+}
+
+class StudentClass implements StudentC {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
