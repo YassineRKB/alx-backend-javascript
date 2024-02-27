382b9a0664d3d7deec32dda177354e3b9bc5577b
YassineRKB <yassine@arcraven.com>
2024-02-28 00:18:05 +0100

    task 8

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const express = require('express');
+const router = express.Router();
+
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+router.get('/', (req, res) => AppController.Home(req, res));
+router.get('/students', (req, res) => StudentsController.getStudents(req, res));
+router.get('/students/:major', (req, res) => StudentsController.getStudentsMajor(req, res));
+
+export default router;
+module.exports = router;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import express from 'express';
+
+const app = express();
+const Router = require('./routes/index');
+const port = 1245;
+
+app.listen(port);
+app.use('/', Router);
+app.use('/students', Router);
+app.use('/students/:major', Router);
+
+export default app;
