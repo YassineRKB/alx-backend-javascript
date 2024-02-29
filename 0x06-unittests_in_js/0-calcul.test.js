0e51de315add092b18b2a57df99443baf9527a8b
YassineRKB <yassine@arcraven.com>
2024-03-01 00:02:14 +0100

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,14 +10,14 @@ describe('calculateNumber', () => {
 
   it('round the second argument', () => {
     assert.equal(calculateNumber(0, 1.0), 1);
-    assert.equal(calculateNumber(0, 1.7), 2);
     assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
 
   it('should return the right number', () => {
     assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(1.3, 1.3), 2);
     assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
     assert.equal(calculateNumber(1.7, 1.2), 3);
     assert.equal(calculateNumber(1.3, 1.8), 3);
     assert.equal(calculateNumber(1.6, 1.8), 4);
