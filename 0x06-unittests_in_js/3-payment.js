1f47d4097e5c318f17f2bf6a01b6544974ce4ce3
YassineRKB <yassine@arcraven.com>
2024-03-01 00:12:53 +0100

    task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const paypay = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${paypay}`);
+}
+  
+module.exports = sendPaymentRequestToApi;
