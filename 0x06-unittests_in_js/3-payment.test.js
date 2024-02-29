a577e3cd7dc1526f0d4c548fa923d4ed77619dd1
YassineRKB <yassine@arcraven.com>
2024-03-01 00:27:12 +0100

    task 3 fi

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,12 +4,12 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    it('should spy on the add function', () => {
-        const spyspy = sinon.spy(Utils, 'calculateNumber')
+  it('should spy on the add function', () => {
+    const spyspy = sinon.spy(Utils, 'calculateNumber')
 
-        sendPaymentRequestToApi(100, 20)
-        sinon.assert.calledWith(spyspy, 'SUM', 100, 20)
-        sinon.assert.calledOnce(spyspy)
-        spyspy.restore()
-    })
+    sendPaymentRequestToApi(100, 20)
+    sinon.assert.calledWith(spyspy, 'SUM', 100, 20)
+    sinon.assert.calledOnce(spyspy)
+    spyspy.restore()
+  })
 })
