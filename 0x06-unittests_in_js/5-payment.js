9a39491f6d480963efbce498937faac4d797fb96
YassineRKB <yassine@arcraven.com>
2024-03-01 00:31:38 +0100

    task 5

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const di = Utils.calculateNumber('SUM', totalAmount, totalShipping)
+    console.log(`The total is: ${di}`)
+}
+
+module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    let di
+    beforeEach(() => {
+        di = sinon.spy(console, 'log');
+      });
+      afterEach(() => {
+        di.restore();
+      });
+      it('should log 120', () => {
+        sendPaymentRequestToApi(100, 20)
+        sinon.assert.calledWith(di, 'The total is: 120');
+        sinon.assert.calledOnce(di)
+      })
+      it('should log 20', () => {
+        sendPaymentRequestToApi(10, 10)
+        sinon.assert.calledWith(di, 'The total is: 20');
+        sinon.assert.calledOnce(di)
+      })
+})
